1abe67c9fea983f66ec7ba14e8a4399d08a83a21
Pulak Raj <praj@email.wm.edu>
2021-03-22 16:29:50 -0400

    passed cases

--- mp520.py
+++ mp520.py
@@ -26,12 +26,13 @@ def get_move_value(state, player, row, column):
             else:
                 counter = 0
                 for i in range(1, len(state)):
-                    if (row + i*x == len(state)) or (column + i*y == len(state)):
+                    if (row + i*x == len(state)) or (column + i*y == len(state)) or (row + i*x < 0) or (column + i*y < 0):
                         break
                     elif state[row + i*x][column + i*y] == other:
                         counter += 1
                     elif state[row + i*x][column + i*y] == player:
                         flipped += counter
+                        break
                     else:
                         break
     return flipped
@@ -59,13 +60,14 @@ def execute_move(state, player, row, column):
             else:
                 flips = []
                 for i in range(1, len(state)):
-                    if (row + i*x == len(state)) or (column + i*y == len(state)):
+                    if (row + i*x == len(state)) or (column + i*y == len(state)) or (row + i*x < 0) or (column + i*y < 0):
                         break
                     elif state[row + i*x][column + i*y] == other:
                         flips.append((row + i*x, column + i*y))
                     elif state[row + i*x][column + i*y] == player:
                         for position in flips:
                             new_state[position[0]][position[1]] = player
+                        break
                     else:
                         break
     new_state[row][column] = player
@@ -104,11 +106,10 @@ def is_terminal_state(state, state_list=None):
         for j in range(len(state)):
             if state[i][j] == " ":
                 if get_move_value(state, "W", i, j) != 0:
-                    terminal = False
+                    return False
                 if get_move_value(state, "B", i, j) != 0:
-                    terminal = False
+                    return False
     return terminal
-
 
 """
 The minimax algorithm. Your implementation should return the best value for the
@@ -122,41 +123,41 @@ def minimax(state, player):
     column = -1
     if (is_terminal_state(state)):
         scores = count_pieces(state)
-        return scores[0] - scores[1]
-    elif player == "B":
+        return (scores[0] - scores[1], row, column)
+    def black():
         maxValue = -10000000
         for i in range(len(state)):
             for j in range(len(state)):
                 if state[i][j] == " ":
-                    print(get_move_value(state, "B", i, j))
                     if get_move_value(state, "B", i, j) > 0:
-                        value = minimax(execute_move(state, "B", i, j), "W")
+                        value = minimax(execute_move(state, "B", i, j), "W")[0]
                         if value > maxValue:
                             maxValue = value
-        # for k in state:
-        #     print(k)
-        # print("")
-        return maxValue
-    elif player == "W":
+                            row = i
+                            column = j
+        if maxValue == -10000000:
+            return white()
+        return (maxValue, row, column)
+    def white():
         minValue = 10000000
         for i in range(len(state)):
             for j in range(len(state)):
                 if state[i][j] == " ":
-                    print(get_move_value(state, "B", i, j))
                     if get_move_value(state, "W", i, j) > 0:
-                        value = minimax(execute_move(state, "W", i, j), "B")
+                        value = minimax(execute_move(state, "W", i, j), "B")[0]
                         if value < minValue:
                             minValue = value
-        # for k in state:
-        #     print(k)
-        # print("")
-        return minValue
+                            row = i
+                            column = j
+        if minValue == 10000000:
+            return black()
+        return (minValue, row, column)
+    if player == "B":
+        return black()
+    if player == "W":
+        return white()
 
-board = [[" ", " ", " ", " "], [" ", "W", "W", " "], ["B", "B", "B", " "], [" ", " ", " ", " "]]
-for i in board:
-    print(i)
-print("")
-print(minimax(board, "B"))
+board = [[" ", " ", " ", " "], ["B", "B", "B", "B"], ["W", "W", "W", "W"], [" ", " ", " ", " "]]
 
 """
 This method should call the minimax algorithm to compute an optimal move sequence
@@ -167,7 +168,21 @@ that leads to an end game.
 def full_minimax(state, player):
     value = 0
     move_sequence = []
-    # Your implementation goes here
+    while(is_terminal_state(state) == False):
+        result = minimax(state, player)
+        if get_move_value(state, player, result[1], result[2]) == 0:
+            if player == "B":
+                player = "W"
+            elif player == "W":
+                player = "B"
+        move_sequence.append((player, result[1], result[2]))
+        state = execute_move(state, player, result[1], result[2])
+        if player == "B":
+            player = "W"
+        elif player == "W":
+            player = "B"
+    value = result[0]
+    move_sequence.append((player,-1, -1))
     return (value, move_sequence)
 
 
@@ -182,9 +197,55 @@ def minimax_ab(state, player, alpha=-10000000, beta=10000000):
     value = 0
     row = -1
     column = -1
-    # Your implementation goes here
-    return (value, row, column)
-
+    if (is_terminal_state(state)):
+        scores = count_pieces(state)
+        return (scores[0] - scores[1], row, column)
+    def black(alpha, beta):
+        maxValue = -10000000
+        flag = False
+        for i in range(len(state)):
+            if flag:
+                break
+            for j in range(len(state)):
+                if state[i][j] == " ":
+                    if get_move_value(state, "B", i, j) > 0:
+                        value = minimax_ab(execute_move(state, "B", i, j), "W", alpha, beta)[0]
+                        if value > maxValue:
+                            maxValue = value
+                            row = i
+                            column = j
+                        alpha = max(alpha, value)
+                        if beta <= alpha:
+                            flag = True
+                            break
+        if maxValue == -10000000:
+            return white(alpha, beta)
+        return (maxValue, row, column)
+    def white(alpha, beta):
+        flag = False
+        minValue = 10000000
+        for i in range(len(state)):
+            if flag:
+                break
+            for j in range(len(state)):
+                if state[i][j] == " ":
+                    if get_move_value(state, "W", i, j) > 0:
+                        value = minimax_ab(execute_move(state, "W", i, j), "B", alpha, beta)[0]
+                        if value < minValue:
+                            minValue = value
+                            row = i
+                            column = j
+                        beta = min(beta, value)
+                        if beta <= alpha:
+                            flag = True
+                            break
+        if minValue == 10000000:
+            return black(alpha, beta)
+        return (minValue, row, column)
+    if player == "B":
+        return black(alpha, beta)
+    if player == "W":
+        return white(alpha, beta)
 
 """
 This method should call the minimax_ab algorithm to compute an optimal move sequence
@@ -195,5 +256,19 @@ that leads to an end game, using alpha-beta pruning.
 def full_minimax_ab(state, player):
     value = 0
     move_sequence = []
-    # Your implementation goes here
+    while(is_terminal_state(state) == False):
+        result = minimax_ab(state, player, alpha=-10000000, beta=10000000)
+        if get_move_value(state, player, result[1], result[2]) == 0:
+            if player == "B":
+                player = "W"
+            elif player == "W":
+                player = "B"
+        move_sequence.append((player, result[1], result[2]))
+        state = execute_move(state, player, result[1], result[2])
+        if player == "B":
+            player = "W"
+        elif player == "W":
+            player = "B"
+    value = result[0]
+    move_sequence.append((player,-1, -1))
     return (value, move_sequence)
